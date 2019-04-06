27334fe1938eb0effe831f37a9b3370f2ce549e0
rishab.dutta@robosoftin.com <rishab.dutta@robosoftin.com>
2019-04-06 19:48:08 +0530

    Initial commit

---
+++ Progress/DownloadManger.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.IO;
+using PShared;
+
+namespace Progress
+{
+    public class DownloadManger
+    {
+
+        public DownloadManger()
+        {
+        }
+
+        public static async Task Main()
+        {
+            //string APIString = "https://rss.itunes.apple.com/api/v1/us/apple-music/hot-tracks/all/100/explicit.json";
+            string APIString = "https://sample-videos.com/zip/10mb.zip";
+
+            var downloadFileUrl = APIString;
+
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var destinationFilePath = Path.Combine(documents, "explicit.json");
+
+            try
+            {
+                
+                using (var client = new HttpClientDownloadWithProgress(downloadFileUrl, destinationFilePath))
+                {
+                    client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) => {
+                        Console.WriteLine($"{progressPercentage}% ({totalBytesDownloaded}/{totalFileSize})");
+                    };
+
+                    await client.StartDownload();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception - ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Progress/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UIKit;
+
+namespace Progress
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            DownloadStuff();
+
+        }
+
+        async void DownloadStuff()
+        {
+            await DownloadManger.Main();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
